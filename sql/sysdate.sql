29fe4251d2f0ae5eda4b1ce2039a3cdf5e8f3a3d
veeraashokcscglobal <157467275+veeraashokcscglobal@users.noreply.github.com>
2024-03-01 15:11:03 +0530

    Sampe for Conflict Mgmt/

--- sql/sysdate.sql
+++ sql/sysdate.sql
@@ -5,4 +5,7 @@ SELECT * FROM ra_customer_trx_all WHERE ROWNUM < 11;
 SELECT * FROM ap_suppliers;
 
 SELECT * FROM ap_supplier_sites_all;
+
+-- Sample for conflict management
+SELECT 'Resource Management' Resources FROM DUAL; 
 /
